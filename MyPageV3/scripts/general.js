57664eafb10f824569d3399072a955a2733d000a
Jack Witt <Jwitt1452@gmail.com>
2019-09-18 13:46:11 -0700

    v3
    - potential design update with a mobile first mindset

---
+++ MyPageV3/scripts/general.js
@@ -0,0 +1,11 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+/*jslint devel: true */
+$(window).ready(function () {
+    "use strict";
+    /*alert("start");
+    $(".page-buffer").animate({opacity: 0}, 1000, function () {
+        this.setAttribute("z-index", "-999");
+        
+    });*/
+});
